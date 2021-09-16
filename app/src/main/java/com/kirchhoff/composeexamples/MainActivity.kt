ec0bb078069651f1f5312df73dcb2cfc31a134d4
Kirchhoff- <kirchhoff.19.92@gmail.com>
2021-09-16 12:37:53 +0300

    Added example of usage NavigationDrawer in Compose.

--- app/src/main/java/com/kirchhoff/composeexamples/MainActivity.kt
+++ app/src/main/java/com/kirchhoff/composeexamples/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.kirchhoff.composeexamples.navigationdrawer.NavigationDrawerActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +15,6 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.bHelloAndroid).setOnClickListener { startActivity(Intent(this, HelloAndroidActivity::class.java)) }
         findViewById<Button>(R.id.bSimpleLayout).setOnClickListener { startActivity(Intent(this, SimpleLayoutActivity::class.java)) }
         findViewById<Button>(R.id.bSimpleCard).setOnClickListener { startActivity(Intent(this, SimpleCardActivity::class.java)) }
+        findViewById<Button>(R.id.bDrawerExample).setOnClickListener { startActivity(Intent(this, NavigationDrawerActivity::class.java)) }
     }
 }
