cc7a61148b47bb540aeb583a5a4dcf64e0750786
Kirchhoff- <kirchhoff.19.92@gmail.com>
2021-10-06 12:45:47 +0300

    Added example of circle offset animation.

--- app/src/main/java/com/kirchhoff/composeexamples/animation/AnimationActivity.kt
+++ app/src/main/java/com/kirchhoff/composeexamples/animation/AnimationActivity.kt
@@ -3,12 +3,12 @@ package com.kirchhoff.composeexamples.animation
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class AnimationActivity: AppCompatActivity() {
 
@@ -21,10 +21,11 @@ class AnimationActivity: AppCompatActivity() {
 @Composable
 @Preview
 fun AnimationUI() {
-    Row(
+    Column(
         modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
        CircleTwinView()
+       CircleOffsetView()
     }
 }
---
+++ app/src/main/java/com/kirchhoff/composeexamples/animation/CircleOffsetView.kt
@@ -0,0 +1,84 @@
+package com.kirchhoff.composeexamples.animation
+
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircleOffsetView() {
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val easing = LinearOutSlowInEasing
+
+    val firstColor by infiniteTransition.animateColor(
+        initialValue = Color(0xFF3f8765),
+        targetValue = Color(0xFFBA3f876),
+        animationSpec = infiniteRepeatable(
+            animation = tween(1500, easing = easing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val secondColor by infiniteTransition.animateColor(
+        initialValue = Color(0xFF546732),
+        targetValue = Color(0xFF987654),
+        animationSpec = infiniteRepeatable(
+            animation = tween(1500, easing = easing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val offsetX by animateValues(
+        values = listOf(0f, 100f, -100f, 0f),
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1500, easing = easing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    val scale by animateValues(
+        values = listOf(1f, 10f, 10f, 10f, 1f),
+        animationSpec = infiniteRepeatable(
+            animation = tween(1500, easing = easing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .size(100.dp)
+        ) {
+            drawCircle(
+                color = Color.White
+            )
+
+            drawCircle(
+                color = secondColor,
+                radius = 80f + scale * 4f,
+                center = Offset(-offsetX + this.center.x, this.center.y)
+            )
+
+            drawCircle(
+                color = firstColor,
+                radius = 80f + scale * 4f,
+                center = Offset(offsetX + this.center.x, this.center.y)
+            )
+        }
+    }
+}
